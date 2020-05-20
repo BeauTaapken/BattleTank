4837bdd676a968377a00785b5a2c1a647851a1b1
BeauTaapken <beautaapken@me.com>
2020-05-20 15:39:05 +0200

    * Main menu * Casting to tank

---
+++ BattleTankGame/Source/BattleTankGame/Public/TankPlayerController.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Tank.h"
+#include "GameFramework/PlayerController.h"
+//Must be last include
+#include "TankPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANKGAME_API ATankPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+public:
+	ATank* GetControlledTank() const;
+
+	void BeginPlay() override;
+};

392df14fd10bb236d8ada9184a3b18170695408e
BTaapken <beau@lioncode.nl>
2020-10-06 16:00:04 +0200

    * Barrel now moves based on mousemovement

--- BattleTankGame/Source/BattleTankGame/Private/TankBarrel.cpp
+++ BattleTankGame/Source/BattleTankGame/Private/TankBarrel.cpp
@@ -6,7 +6,10 @@
 void UTankBarrel::Elevate(float RelativeSpeed)
 {
 	//Move the barrel the right amount this frame
-
-	
 	//Given a max elevation speed and frame time
+	RelativeSpeed = FMath::Clamp<float>(RelativeSpeed, -1, 1);
+	auto ElevationChange = RelativeSpeed * MaxDegreesPerSecond * GetWorld()->DeltaTimeSeconds;
+	auto RawNewElevation = RelativeRotation.Pitch + ElevationChange;
+	auto NewElevation = FMath::Clamp<float>(RawNewElevation, MinElevationDegrees, MaxElevationDegrees);
+	SetRelativeRotation(FRotator(NewElevation, 0, 0));
 }

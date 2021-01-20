5e7fd612ef9e65eed555383cca2f46a2aeb09fef
BTaapken <beau@lioncode.nl>
2021-01-20 11:04:31 +0100

    * Projectiles now launch after spawning from tanks

--- BattleTankGame/Source/BattleTankGame/Private/Tank.cpp
+++ BattleTankGame/Source/BattleTankGame/Private/Tank.cpp
@@ -50,15 +50,15 @@ void ATank::SetTurretReference(UTankTurret * TurretToSet)
 
 void ATank::Fire()
 {
-	UE_LOG(LogTemp, Warning, TEXT("Tank Fired"));
-
 	if (!Barrel) { return; }
 
 	// Spawn a projectile at the socket location of the barrel
-	GetWorld()->SpawnActor<AProjectile>(
+	auto Projectile = GetWorld()->SpawnActor<AProjectile>(
 		ProjectileBlueprint,
 		Barrel->GetSocketLocation(FName("Projectile")),
 		Barrel->GetSocketRotation(FName("Projectile"))
 	);
+
+	Projectile->LaunchProjectile(LaunchSpeed);
 }
 
--- BattleTankGame/Source/BattleTankGame/Public/Projectile.h
+++ BattleTankGame/Source/BattleTankGame/Public/Projectile.h
@@ -4,23 +4,29 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "GameFramework/ProjectileMovementComponent.h"
 #include "Projectile.generated.h"
 
 UCLASS()
 class BATTLETANKGAME_API AProjectile : public AActor
 {
 	GENERATED_BODY()
-	
-public:	
+
+public:
 	// Sets default values for this actor's properties
 	AProjectile();
+	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	void LaunchProjectile(float Speed);
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+private:
+	UProjectileMovementComponent* ProjectileMovement = nullptr;
+
 
 };

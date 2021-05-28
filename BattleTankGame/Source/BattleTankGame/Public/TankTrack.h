292181c6dea440e1e92a77ac8fe1cb74ae743e7a
BeauTaapken <beau@lioncode.nl>
2021-05-28 08:59:31 +0200

    * Tanks now rotate barrel the shortest way to aimingpoint

--- BattleTankGame/Source/BattleTankGame/Public/TankTrack.h
+++ BattleTankGame/Source/BattleTankGame/Public/TankTrack.h
@@ -27,9 +27,13 @@ private:
 	UTankTrack();
 
 	virtual void BeginPlay() override;
-	
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
 	UFUNCTION()
 	void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit);
+
+	void ApplySidewaysForce();
+	
+	void DriveTrack();
+
+	float CurrentThrottle = 0;
 };
